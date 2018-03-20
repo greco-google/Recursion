268e1602619963783834ef901d99ddcbd7cf8352
Josh Greco <greco.joshua@gmail.com>
2018-03-20 17:11:21 -0400

    Setting up/working

--- src/BaseCasevsIterateCase/Example5.java
+++ src/BaseCasevsIterateCase/Example5.java
@@ -1,5 +1,17 @@
 package BaseCasevsIterateCase;
 
 public class Example5 {
+	
+	public int recur(int k, int n) {
+		if (n == k) {
+			return k;
+		} else {
+			if (n > k) {
+				return recur(k, n - k);
+			} else {
+				return recur(k - n, n);
+			}
+		}
+	}
 
 }
--- src/BaseCasevsIterateCase/Example6.java
+++ src/BaseCasevsIterateCase/Example6.java
@@ -1,5 +1,13 @@
 package BaseCasevsIterateCase;
 
 public class Example6 {
+	
+	public int result(int n) {
+		if(n==1) {
+			return 2;
+		} else {
+			return 2 * result(n-1);
+		}
+	}
 
 }

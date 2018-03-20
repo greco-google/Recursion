42d9e292d27029ef56cec13d109ea7226a241f65
Josh Greco <greco.joshua@gmail.com>
2018-03-20 17:20:44 -0400

    Minor bug fixes

--- src/BaseCasevsIterateCase/RecursiveMethods.java
+++ src/BaseCasevsIterateCase/RecursiveMethods.java
@@ -4,26 +4,26 @@ public class RecursiveMethods {
 	
 	//Example 1
 	public int mystery(int n) {
-		if(n<0) {
-			return n*2;
+		if(n < 0) {
+			return n * 2;
 		} else {
-			return mystery(n-1) + mystery(n-2);
+			return mystery(n - 1) + mystery(n - 2);
 		}
 	}
 	
 	//Example 2
 	public int mystery(int n, int a, int b) {
-		if(n==0) {
+		if(n == 0) {
 			return a;
 		} else {
-			return b* mystery(n-1, a, b);
+			return b * mystery(n - 1, a, b);
 		}
 	}
 	
 	//Example 3
 	public int recur(int n) {
 		if(n <= 10) {
-			return n*2;
+			return n * 2;
 		} else {
 			return recur(recur(n/3));
 		}
@@ -31,10 +31,10 @@ public class RecursiveMethods {
 	
 	//Example 4
 	public int recur(int n, int a, int b) {
-		if(n==1) {
+		if(n == 1) {
 			return a;
 		} else {
-			return b + recur(n-1, a, b);
+			return b + recur(n - 1, a, b);
 		}
 	}
 	
@@ -53,10 +53,10 @@ public class RecursiveMethods {
 	
 	//Example 6
 	public int result(int n) {
-		if(n==1) {
+		if(n == 1) {
 			return 2;
 		} else {
-			return 2 * result(n-1);
+			return 2 * result(n - 1);
 		}
 	}
 
